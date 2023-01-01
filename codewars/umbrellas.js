039010364121a7d870743dd43ab9adb5bf5ac1d8
wen <owilalala974@gmail.com>
2023-01-01 22:56:34 +0800

    完成雨傘題

--- codewars/umbrellas.js
+++ codewars/umbrellas.js
@@ -13,46 +13,48 @@
 //輸入一個陣列，arr[奇數是早上], arr[偶數是下午], [sunny, sunny] -> 一天天氣
 
 function minUmbrellas(weather) {
-    let newWeather = weather.map((v, i) => {
-        return { weather: v, index: i }
+    let badHalfDay = weather.filter((v) => {
+        return v === 'rainy' || v === 'thunderstorms'
     })
-    let sameBadDay = newWeather.filter((v) => {
-        return v.weather === 'rainy' || v.weather === 'thunderstorms'
-    })
-    let newMap = sameBadDay.map((v,i)=>{
-        return v.index
-    })
-
-    //上午下午 index+1 的一支傘
-    let reCount = newMap.filter((v,i,arr)=>{
-      return v+1 === arr[i+1]
-    })
-    console.log(reCount.length)
-    
-    //偶數奇數
-    let even = [];
-    let odd = [];
-    newMap.forEach((num) => {
-        if (Math.abs(num % 2) === 0) {
-            even.push(num)
-        } else {
-            odd.push(num)
-        }
-    })
-    console.log(even, odd)
-    // return (even.length > odd.length) ? odd[0] : even[0]
-
-    if (sameBadDay.length === 0) {
+    if (badHalfDay.length === 0) {
+        console.log('All good days')
         return 0
     }
-    if (sameBadDay.length === weather.length) {
+    if (badHalfDay.length === weather.length) {
+        console.log('All bad days')
         return 1
     }
 
-    return sameBadDay.length - reCount.length
-
+    let takeUmbrellaGoCompany = 0;
+    let takeUmbrellaGoHome = 0;
+    for (let i = 0; i <= weather.length; i++) {
+        if (i % 2 === 0) {
+            if (weather[i] === 'rainy' || weather[i] === 'thunderstorms') {
+                if (takeUmbrellaGoHome === 0) {
+                    takeUmbrellaGoCompany++
+                }else{
+                  takeUmbrellaGoHome--
+                  takeUmbrellaGoCompany++
+                }
+            }
+        } else {
+            if (weather[i] === 'rainy' || weather[i] === 'thunderstorms') {
+                if (takeUmbrellaGoCompany === 0) {
+                    takeUmbrellaGoHome++
+                }else{
+                  takeUmbrellaGoCompany--
+                  takeUmbrellaGoHome++
+                }
+            }
+        }
+    }
+    return takeUmbrellaGoCompany + takeUmbrellaGoHome
 }
 
-minUmbrellas(['sunny','thunderstorms','windy','thunderstorms','sunny','rainy','clear','sunny','thunderstorms','windy' ])
+
+
+minUmbrellas(['sunny', 'sunny', 'cloudy', 'cloudy', 'rainy', 'cloudy', 'rainy', 'clear', 'sunny', 'thunderstorms'])
 //["rainy", "clear", "thunderstorms", "cloudy"]
 //["clear"]
+// ['sunny', 'thunderstorms', 'windy', 'thunderstorms', 'sunny', 'rainy', 'clear', 'sunny', 'thunderstorms', 'windy']
+//['sunny','sunny','cloudy','cloudy','rainy','cloudy','rainy','clear','sunny','thunderstorms' ]
